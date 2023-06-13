eaaa341ca16de5a6d38539d8c7a6332bc8fa25d2
alperencubuk <alperencubuk@outlook.com>
2023-06-13 16:50:15 +0300

    Added traverse folder logic

--- source/app/onedrive/onedrive.py
+++ source/app/onedrive/onedrive.py
@@ -1,4 +1,4 @@
-from os import makedirs, path
+from os import getcwd, makedirs
 
 # Third Party Packages
 from O365 import Account, FileSystemTokenBackend
@@ -20,13 +20,14 @@ class OneDriveStorage:
             token_backend=self.token_backend,
         )
         self.state = user_id
+        self.download_path = f"{getcwd()}/files/{user_id}"
 
     def get_auth_url(self) -> str:
         self.account.con.session = oauth = self.account.con.get_session(
             redirect_uri=self.callback,
             scopes=["User.Read", "Files.Read.All", "offline_access"],
         )
-        auth_url, state = oauth.authorization_url(
+        auth_url, _ = oauth.authorization_url(
             url=self.account.con._oauth2_authorize_url,
             state=self.state,
             access_type="offline",
@@ -52,25 +53,37 @@ class OneDriveStorage:
     def get_file_list(self) -> dict:
         drive = self.account.storage().get_default_drive()
         items = []
-        for item in drive.get_root_folder().get_items():
-            if item.is_folder:
-                for folder_item in item.get_items():
-                    items.append(f"{item.name}/{folder_item.name}")
-            else:
-                items.append(item.name)
-        return {"items": items}
+
+        def traverse_folder(folder, path: str = ""):
+            nonlocal items
+            for item in folder.get_items():
+                if item.is_folder:
+                    new_path = f"{path}/{item.name}" if path else item.name
+                    traverse_folder(item, path=new_path)
+                else:
+                    file_path = f"{path}/{item.name}" if path else item.name
+                    items.append(file_path)
+
+        root_folder = drive.get_root_folder()
+        traverse_folder(root_folder)
+        return {"files": items}
 
     def download_all(self) -> None:
-        download_path = path.abspath(
-            path.join(path.dirname(__file__), "../../..", "files", self.state)
-        )
-        makedirs(download_path, exist_ok=True)
         drive = self.account.storage().get_default_drive()
-        for item in drive.get_root_folder().get_items():
-            if item.is_folder:
-                for folder_item in item.get_items():
-                    makedirs(f"{download_path}/{item.name}", exist_ok=True)
-                    folder_item.download(f"{download_path}/{item.name}")
-            else:
-                item.download(download_path)
+        makedirs(self.download_path, exist_ok=True)
+
+        def traverse_folder(folder, path: str = "") -> None:
+            for item in folder.get_items():
+                if item.is_folder:
+                    new_path = f"{path}/{item.name}" if path else item.name
+                    traverse_folder(item, path=new_path)
+                else:
+                    new_path = (
+                        f"{self.download_path}/{path}" if path else self.download_path
+                    )
+                    makedirs(new_path, exist_ok=True)
+                    item.download(new_path)
+
+        root_folder = drive.get_root_folder()
+        traverse_folder(root_folder)
         print("Download Completed")
